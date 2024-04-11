cd176a7f32b1a0a7c142042182f74600ac25af99
yibei333 <Developer333@hotmail.com>
2024-04-11 17:48:48 +0800

    add menus

--- src/ProgrammerTools/MauiProgram.cs
+++ src/ProgrammerTools/MauiProgram.cs
@@ -1,6 +1,5 @@
 ﻿using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
-using Microsoft.Maui.LifecycleEvents;
 
 namespace ProgrammerTools;
 
@@ -11,31 +10,10 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
-            .UseMauiCommunityToolkit()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            })
-            .ConfigureLifecycleEvents(events =>
-            {
-#if WINDOWS
-                events.AddWindows(windowsLifecycleBuilder =>
-                {
-                    windowsLifecycleBuilder.OnWindowCreated(window =>
-                    {
-                        window.ExtendsContentIntoTitleBar = true;
-                        var bar = new Microsoft.UI.Xaml.Controls.StackPanel { Height=50,Background=new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.Red) };
-                        var content = new Microsoft.UI.Xaml.Controls.TextBlock { Text = "aa" };
-                        bar.Children.Add(content);
-                        window.SetTitleBar(bar);
-                        window.Title = "aaaa";
-                    });
-                });
-#endif
-            });
+            .UseMauiCommunityToolkit();
 
         builder.Services.AddMauiBlazorWebView();
+
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
         builder.Logging.AddDebug();
--- src/ProgrammerTools/wwwroot/components/layout/layout.js
+++ src/ProgrammerTools/wwwroot/components/layout/layout.js
@@ -1,7 +1,32 @@
 export default {
+    data() {
+        return {
+            isCollapse: false,
+            activedId: "1",
+            routes: [],
+            menus: [],
+            tabs: [
+                {
+                    label: '主页',
+                    key: '1',
+                },
+            ],
+        }
+    },
+    mounted() {
+        this.menus = pagesConfig;
+        this.routes = getVueRotes();
+    },
     methods: {
         test(name) {
             this.$router.push({ name: name });
+        },
+        handleSelect(id) {
+            let route = this.routes.filter(x => x.id == id);
+            let name = (route && route.length > 0) ? route[0].name : null;
+            if (name) {
+                this.$router.push({ name: name });
+            }
         }
     }
 }
