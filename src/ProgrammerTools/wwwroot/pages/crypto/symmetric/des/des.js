d756dfa04e90dd654028f65ae1c5f3cc89d914a9
yibei333 <Developer333@hotmail.com>
2024-04-24 18:29:47 +0800

    clean code

--- src/ProgrammerTools/wwwroot/pages/crypto/symmetric/des/des.js
+++ src/ProgrammerTools/wwwroot/pages/crypto/symmetric/des/des.js
@@ -1,7 +1,6 @@
 export default {
     data() {
         return {
-            test:12
         }
     },
     mounted() {
--- src/ProgrammerTools/wwwroot/pages/home/home.js
+++ src/ProgrammerTools/wwwroot/pages/home/home.js
@@ -1,17 +1,12 @@
 export default {
     data() {
         return {
-            message: '',
-            files: []
         }
     },
     mounted() {
         this.$router.push({ name: 'aes' });
     },
     methods: {
-        test() {
-            this.$Message.info('This is a info tip');
-            this.message = this.$t('message.currentVersion');
-        }
+        
     }
 }
