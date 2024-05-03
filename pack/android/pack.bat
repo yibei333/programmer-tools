7d37984d4eb048843bbbb4048318925ca60d0d83
developer333 <Developer333@hotmail.com>
2024-05-03 23:00:56 +0800

    pack

--- pack/android/pack.bat
+++ pack/android/pack.bat
@@ -14,9 +14,9 @@ IF EXIST "%binaryPath%" (
 cd "%cd%"
 git pull
 
-dotnet publish "%projectPath%" -o "%binaryPath%" -f net8.0-android -c Release -p:AndroidSigningKeyStore="%cd%\demo.keystore" -p:AndroidSigningKeyAlias=demo123 -p:AndroidSigningKeyPass=demo123 -p:AndroidSigningStorePass=demo123
+dotnet publish "%projectPath%" --artifacts-path "%binaryPath%" -o "%binaryPath%" -f net8.0-android -c Release -p:ApplicationVersion="1" -p:ApplicationDisplayVersion="%version%" -p:ApplicationTitle="ProgrammerTools" -p:ApplicationId="com.yibei333.programmertools" -p:AndroidKeyStore=true -p:AndroidSigningKeyStore="%cd%\demo.keystore" -p:AndroidSigningKeyAlias=demo123 -p:AndroidSigningKeyPass=demo123 -p:AndroidSigningStorePass=demo123
 
-copy %binaryPath%\com.yibei.programmertools.apk "%packagePath%\ProgrammerTools.android.%version%.apk"
+copy "%binaryPath%\com.yibei333.programmertools-Signed.apk" "%packagePath%\ProgrammerTools.android.%version%.apk"
 
 IF EXIST "%binaryPath%" (
     rd /s /q "%binaryPath%"
